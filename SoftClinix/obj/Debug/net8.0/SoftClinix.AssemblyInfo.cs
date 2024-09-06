7be749647e7afaeec88b46c0da4619426aa6a7ea
1Armagedon0 <144762539+1Armagedon0@users.noreply.github.com>
2024-09-06 04:11:50 -0600

    AD

--- SoftClinix/obj/Debug/net8.0/SoftClinix.AssemblyInfo.cs
+++ SoftClinix/obj/Debug/net8.0/SoftClinix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftClinix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7523493d146c6d85be3a388e415893d9f47b5191")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b625d5ee1faaa5e2edf249658bf0f06570cf80")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftClinix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftClinix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
